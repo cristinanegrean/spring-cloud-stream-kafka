1466fe15dcadf03151b3ec9a6dc433806a71dfd9
cristinanegrean <negrean.cristina@gmail.com>
2017-07-27 13:51:59 +0200

    fix build, forgot to add new file Rating.java to git

---
+++ src/main/java/cristina/tech/worker/domain/Rating.java
@@ -0,0 +1,23 @@
+package cristina.tech.worker.domain;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+/** JSON Payload of {@link cristina.tech.worker.event.DressRatedEvent}. */
+@Data
+@NoArgsConstructor(force = true) //Jackson JSON needs this for deserialization
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Rating implements Serializable {
+
+    private String dressId, ratingId;
+    private Integer stars;
+
+}
