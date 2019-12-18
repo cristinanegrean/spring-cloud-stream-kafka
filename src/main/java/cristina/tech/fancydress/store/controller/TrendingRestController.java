636e311bf4505a582e0df5483186ed7f7ad77582
cristinanegrean <hello@cristina.tech>
2019-12-18 17:19:23 +0100

    add method parameter to @RequestMapping annotation to solve cwe, owasp reported vulnerability in sonarqube

--- src/main/java/cristina/tech/fancydress/store/controller/TrendingRestController.java
+++ src/main/java/cristina/tech/fancydress/store/controller/TrendingRestController.java
@@ -5,10 +5,7 @@ import cristina.tech.fancydress.store.view.DressDetailView;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.regex.Matcher;
@@ -28,7 +25,7 @@ public class TrendingRestController {
     @Autowired
     private TrendingDressesService trendingDressesService;
 
-    @RequestMapping(value = "/trending")
+    @RequestMapping(path = "/trending", method = RequestMethod.GET)
     public List<DressDetailView> trending(
             @RequestParam(value="count", defaultValue=COUNT_DEFAULT) Integer count,
             @RequestParam(value="interval", defaultValue=INTERVAL_DEFAULT) String interval) {
